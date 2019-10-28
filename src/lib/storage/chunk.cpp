685a0770ca37afcebe60a8b1da68dc2f61d98ec0
Julius Streit <julius.streit@student.hpi.de>
2019-10-28 14:25:20 +0100

    Completes task 3 and 4. Adds a Test for size to detect Segfault when size function is broken.

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -15,26 +15,33 @@
 namespace opossum {
 
 void Chunk::add_segment(std::shared_ptr<BaseSegment> segment) {
-  // Implementation goes here
+  _segments.push_back(segment);
 }
 
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
-  // Implementation goes here
+	Assert(values.size() == _segments.size(), "Incorrect Data rows vs segment Rows");
+	for (size_t curSeg = 0; curSeg < _segments.size(); curSeg++){
+		_segments[curSeg]->append(values[curSeg]);
+	}
+  
 }
 
 std::shared_ptr<BaseSegment> Chunk::get_segment(ColumnID column_id) const {
-  // Implementation goes here
-  return nullptr;
+  // Shouldn't this be segment ID? 
+  return _segments[column_id];
 }
 
 uint16_t Chunk::column_count() const {
-  // Implementation goes here
-  return 0;
+  return _segments.size();
 }
 
 uint32_t Chunk::size() const {
-  // Implementation goes here
-  return 0;
+	if (column_count() > 0){
+  		return _segments.front()->size();
+  	}
+  	else{
+  	return 0;
+  	}
 }
 
 }  // namespace opossum

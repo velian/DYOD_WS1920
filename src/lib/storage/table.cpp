52fd78ef7f3ec357e2de5ac333c5a71d3d0dc2dd
Leonard Pabst <leopab@gmx.de>
2019-11-11 12:16:46 +0100

    started compress_chunk

--- src/lib/storage/table.cpp
+++ src/lib/storage/table.cpp
@@ -15,6 +15,7 @@
 #include "resolve_type.hpp"
 #include "types.hpp"
 #include "utils/assert.hpp"
+#include "dictionary_segment.hpp"
 
 namespace opossum {
 
@@ -76,6 +77,17 @@ void Table::emplace_chunk(Chunk chunk) {
 
 void Table::_add_chunk() { _chunks.push_back(std::make_shared<Chunk>()); }
 
-void Table::compress_chunk(ChunkID chunk_id) { throw std::runtime_error("Implement Table::compress_chunk"); }
+void Table::compress_chunk(ChunkID chunk_id) { 
+  const auto& uncompressed_chunk = get_chunk(chunk_id);
+  Chunk compressed_chunk = Chunk();
+
+  u_int32_t number_of_segments = uncompressed_chunk.size;
+  for (ColumnID segment_ID = ColumnID{0}; segment_ID < number_of_segments; segment_ID++) {
+    std::shared_ptr<BaseSegment> value_segment = uncompressed_chunk.get_segment(segment_ID);
+    //std::shared_ptr<BaseSegment> dictionary_segment = DictionarySegment(value_segment);
+
+    //compressed_chunk.add_segment(dictionary_segment)
+  }
+ }
 
 }  // namespace opossum

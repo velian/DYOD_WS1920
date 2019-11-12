0535a1ca3f51a8d85baf98c16df653abf6bc2d34
Julius Streit <julius.streit@student.hpi.de>
2019-11-12 22:02:25 +0100

    Formats, Lints

--- src/lib/storage/dictionary_segment.hpp
+++ src/lib/storage/dictionary_segment.hpp
@@ -5,10 +5,11 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <algorithm>
 
 #include "all_type_variant.hpp"
-#include "types.hpp"
 #include "fixed_size_attribute_vector.hpp"
+#include "types.hpp"
 
 namespace opossum {
 
@@ -24,35 +25,32 @@ template <typename T>
 class DictionarySegment : public BaseSegment {
  public:
   /**
-   * Creates a Dictionary segment from a given value segment.
+   *  Creates a Dictionary segment from a given value segment.
    */
-  explicit DictionarySegment(const std::shared_ptr<BaseSegment>& base_segment){
-
+  explicit DictionarySegment(const std::shared_ptr<BaseSegment>& base_segment) {
     auto segment = std::dynamic_pointer_cast<ValueSegment<T>>(base_segment);
     auto segment_values = segment->values();
 
-    //create dictionary
-    _dictionary = std::make_shared<std::vector<T>> (segment_values);
+    // create dictionary
+    _dictionary = std::make_shared<std::vector<T>>(segment_values);
     std::sort(_dictionary->begin(), _dictionary->end());
     _dictionary->erase(std::unique(_dictionary->begin(), _dictionary->end()), _dictionary->end());
 
-    //create attribute vector with minimal width
-    if(unique_values_count() <= UINT8_MAX) {
-      _attribute_vector = std::make_shared<FixedSizeAttributeVector<uint8_t>> (segment->size());
-    }
-    else if(unique_values_count() <= UINT16_MAX) {
-      _attribute_vector = std::make_shared<FixedSizeAttributeVector<uint16_t>> (segment->size());
-    }
-    else {
-      _attribute_vector = std::make_shared<FixedSizeAttributeVector<uint32_t>> (segment->size());
+    // create attribute vector with minimal width
+    if (unique_values_count() <= UINT8_MAX) {
+      _attribute_vector = std::make_shared<FixedSizeAttributeVector<uint8_t>>(segment->size());
+    } else if (unique_values_count() <= UINT16_MAX) {
+      _attribute_vector = std::make_shared<FixedSizeAttributeVector<uint16_t>>(segment->size());
+    } else {
+      _attribute_vector = std::make_shared<FixedSizeAttributeVector<uint32_t>>(segment->size());
     }
 
-    //fill attribute vector with valueIDs
-    for(ValueID position(0); position < segment->size(); position++) {
-      auto value_id = ValueID(std::distance(_dictionary->begin(), std::find(_dictionary->begin(), _dictionary->end(), segment_values.at(position))));
+    // fill attribute vector with valueIDs
+    for (ValueID position(0); position < segment->size(); position++) {
+      auto value_id = ValueID(std::distance(
+          _dictionary->begin(), std::find(_dictionary->begin(), _dictionary->end(), segment_values.at(position))));
       _attribute_vector->set(position, value_id);
     }
-
   }
 
   // SEMINAR INFORMATION: Since most of these methods depend on the template parameter, you will have to implement
@@ -64,68 +62,51 @@ class DictionarySegment : public BaseSegment {
   }
 
   // return the value at a certain position.
-  T get(const size_t chunk_offset) const{
-    return value_by_value_id(ValueID(_attribute_vector->get(chunk_offset)));
-  }
+  T get(const size_t chunk_offset) const { return value_by_value_id(ValueID(_attribute_vector->get(chunk_offset))); }
 
   // dictionary segments are immutable
-  void append(const AllTypeVariant&) override {
-  }
+  void append(const AllTypeVariant&) override {}
 
   // returns an underlying dictionary
-  std::shared_ptr<const std::vector<T>> dictionary() const{
-    return _dictionary;
-  }
+  std::shared_ptr<const std::vector<T>> dictionary() const { return _dictionary; }
 
   // returns an underlying data structure
-  std::shared_ptr<const BaseAttributeVector> attribute_vector() const{
-    return _attribute_vector;
-  }
+  std::shared_ptr<const BaseAttributeVector> attribute_vector() const { return _attribute_vector; }
 
   // return the value represented by a given ValueID
-  const T& value_by_value_id(ValueID value_id) const{
-    return _dictionary->at(value_id);
-  }
+  const T& value_by_value_id(ValueID value_id) const { return _dictionary->at(value_id); }
 
   // returns the first value ID that refers to a value >= the search value
   // returns INVALID_VALUE_ID if all values are smaller than the search value
   ValueID lower_bound(T value) const {
     auto lower_bound = std::lower_bound(_dictionary->begin(), _dictionary->end(), value);
-    if(lower_bound == _dictionary->end()) {
+    if (lower_bound == _dictionary->end()) {
       return INVALID_VALUE_ID;
     }
     return ValueID(std::distance(_dictionary->begin(), lower_bound));
   }
 
   // same as lower_bound(T), but accepts an AllTypeVariant
-  ValueID lower_bound(const AllTypeVariant& value) const {
-    return lower_bound(static_cast<T> (value));
-  }
+  ValueID lower_bound(const AllTypeVariant& value) const { return lower_bound(static_cast<T>(value)); }
 
   // returns the first value ID that refers to a value > the search value
   // returns INVALID_VALUE_ID if all values are smaller than or equal to the search value
   ValueID upper_bound(T value) const {
     auto upper_bound = std::upper_bound(_dictionary->begin(), _dictionary->end(), value);
-    if(upper_bound == _dictionary->end()) {
+    if (upper_bound == _dictionary->end()) {
       return INVALID_VALUE_ID;
     }
     return ValueID(std::distance(_dictionary->begin(), upper_bound));
   }
 
   // same as upper_bound(T), but accepts an AllTypeVariant
-  ValueID upper_bound(const AllTypeVariant& value) const {
-    return upper_bound(static_cast<T> (value));
-  }
+  ValueID upper_bound(const AllTypeVariant& value) const { return upper_bound(static_cast<T>(value)); }
 
   // return the number of unique_values (dictionary entries)
-  size_t unique_values_count() const{
-    return _dictionary->size();
-  }
+  size_t unique_values_count() const { return _dictionary->size(); }
 
   // return the number of entries
-  size_t size() const override{
-    return _attribute_vector->size();
-  }
+  size_t size() const override { return _attribute_vector->size(); }
 
   // returns the calculated memory usage
   size_t estimate_memory_usage() const final {

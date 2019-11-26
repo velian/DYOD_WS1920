4c1196b7261f1614bbf182f60572d491fd533651
Julius Streit <julius.streit@student.hpi.de>
2019-11-26 22:31:50 +0100

    refseg impl

--- src/lib/storage/reference_segment.hpp
+++ src/lib/storage/reference_segment.hpp
@@ -34,6 +34,12 @@ class ReferenceSegment : public BaseSegment {
   const std::shared_ptr<const Table> referenced_table() const;
 
   ColumnID referenced_column_id() const;
+
+ private:
+  const std::shared_ptr<const Table> _referenced_table;
+  const ColumnID _referenced_column_id;
+  const std::shared_ptr<const PosList> _pos_list;
+
 };
 
 }  // namespace opossum

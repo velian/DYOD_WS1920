b07c180f6ccf53f32879e84d7568a0e1997d7190
carla.terboven <carla.terboven@student.hpi.de>
2019-11-26 12:11:05 +0100

    Implement get_comparator

--- src/lib/operators/table_scan.cpp
+++ src/lib/operators/table_scan.cpp
@@ -26,47 +26,77 @@ ScanType TableScan::scan_type() const {
 
 const AllTypeVariant& TableScan::search_value() const {
   return _search_value;
+
 }
 
 auto get_comparator(ScanType scanType) {
+  return [scanType](const auto left, const auto right) {
     switch (scanType) {
       case ScanType::OpEquals: {
-        return [](auto left, auto right) { return left == right; };
-        break;
+        return left == right;
       }
       case ScanType::OpNotEquals: {
-        return [](auto left, auto right) { return left != right; };
-        break;
+        return left != right;
       }
       case ScanType::OpLessThan: {
-        return [](auto left, auto right) { return left < right; };
-        break;
+        return left < right;
       }
       case ScanType::OpLessThanEquals: {
-        return [](auto left, auto right) { return left <= right; };
-        break;
+        return left <= right;
       }
       case ScanType::OpGreaterThan: {
-        return [](auto left, auto right) { return left > right; };
-        break;
+        return left > right;
       }
       case ScanType::OpGreaterThanEquals: {
-        return [](auto left, auto right) { return left >= right; };
-        break;
+        return left >= right;
       }
-      //bdefault: break;
+     //bdefault: break;
     }
   };
+}
+
+/*
+auto get_comparator(ScanType scanType) {
+  switch (scanType) {
+    case ScanType::OpEquals: {
+      return [](const auto left, const auto right) -> bool { return left == right; };
+      break;
+    }
+    case ScanType::OpNotEquals: {
+      return [](const auto left, const auto right) -> bool { return left != right; };
+      break;
+    }
+    case ScanType::OpLessThan: {
+      return [](const auto left, const auto right) -> bool { return left < right; };
+      break;
+    }
+    case ScanType::OpLessThanEquals: {
+      return [](const auto left, const auto right) -> bool { return left <= right; };
+      break;
+    }
+    case ScanType::OpGreaterThan: {
+      return [](const auto left, const auto right) -> bool { return left > right; };
+      break;
+    }
+    case ScanType::OpGreaterThanEquals: {
+      return [](const auto left, const auto right) -> bool { return left >= right; };
+      break;
+    }
+   //bdefault: break;
+  }
+};
+*/
+
 
 std::shared_ptr<const Table> TableScan::_on_execute() {
-  const auto table = _input_table_left;
+  const auto table = _input_table_left();
   auto comparator = get_comparator(_scan_type);
   //for (auto idx = 0; idx < value_segment.size(); ++idx) {
-
+    //  return comparator(value_segment.get(idx), search_value());
   //}
 
-  for (int i = 0; i < _input_table_left()->chunk_count; i++) {
-    const auto& chunk = _input_table_left()->get_chunk(ChunkID(i));
+  for (int i = 0; i < table->chunk_count; i++) {
+    const auto& chunk = table->get_chunk(ChunkID(i));
     const auto& segment = chunk.get_segment(_column_id);
 
     // Kann man den Typen direkt abfragen, dass man hier keinen vector
@@ -74,16 +104,17 @@ std::shared_ptr<const Table> TableScan::_on_execute() {
 
     std::vector<std::string> possible_types{"int", "long", "float", "double", "string"};
     for(auto data_type : possible_types) {
-      resolve_data_type(data_type, [&] (auto type)) {
+      resolve_data_type(data_type, [&] (auto type) {
         using Type = typename decltype(type)::type;
         const auto value_segment = std::dynamic_pointer_cast<ValueSegment>(segment);
         if(value_segment != nullptr) {
-          const auto& values = value_segment->values();
-          for (auto value : values) {
-            if ()
+          const auto values = value_segment->values();
+          for (auto& value : values) {
+            //if ()
           }
         }
-      }
+        return;
+      });
     }
     
   }

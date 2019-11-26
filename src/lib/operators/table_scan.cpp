859cd6317a9347d2581f8212a74948dd551c6c2d
Leonard Pabst <leopab@gmx.de>
2019-11-26 15:03:37 +0100

    add ValueSegment scan

--- src/lib/operators/table_scan.cpp
+++ src/lib/operators/table_scan.cpp
@@ -28,64 +28,101 @@ const AllTypeVariant& TableScan::search_value() const {
   return _search_value;
 }
 
-auto get_comparator(ScanType scanType) {
-    switch (scanType) {
-      case ScanType::OpEquals: {
-        return [](auto left, auto right) { return left == right; };
+/*auto get_comparator(ScanType scanType) {
+  switch (scanType) {
+    case ScanType::OpEquals: {
+      return [](auto left, auto right) { return left == right; };
+      break;
+    }
+    case ScanType::OpNotEquals: {
+      return [](auto left, auto right) { return left != right; };
+      break;
+    }
+    case ScanType::OpLessThan: {
+      return [](auto left, auto right) { return left < right; };
+      break;
+    }
+    case ScanType::OpLessThanEquals: {
+      return [](auto left, auto right) { return left <= right; };
+      break;
+    }
+    case ScanType::OpGreaterThan: {
+      return [](auto left, auto right) { return left > right; };
+      break;
+    }
+    case ScanType::OpGreaterThanEquals: {
+      return [](auto left, auto right) { return left >= right; };
+      break;
+    }
+    //default: break;
+  }
+}*/
+
+std::shared_ptr<const Table> TableScan::_on_execute() {
+  const auto table = _input_table_left();
+  //auto comparator = get_comparator(_scan_type);
+  auto comparator = [](ScanType scanType, auto left, auto right) {
+  switch (scanType) {
+    case ScanType::OpEquals: {
+        return left == right;
         break;
       }
       case ScanType::OpNotEquals: {
-        return [](auto left, auto right) { return left != right; };
+        return left != right;
         break;
       }
       case ScanType::OpLessThan: {
-        return [](auto left, auto right) { return left < right; };
+        return left < right;
         break;
       }
       case ScanType::OpLessThanEquals: {
-        return [](auto left, auto right) { return left <= right; };
+        return left <= right;
         break;
       }
       case ScanType::OpGreaterThan: {
-        return [](auto left, auto right) { return left > right; };
+        return left > right;
         break;
       }
       case ScanType::OpGreaterThanEquals: {
-        return [](auto left, auto right) { return left >= right; };
+        return left >= right;
         break;
       }
-      //bdefault: break;
+      //default: break;
     }
   };
 
-std::shared_ptr<const Table> TableScan::_on_execute() {
-  const auto table = _input_table_left;
-  auto comparator = get_comparator(_scan_type);
-  //for (auto idx = 0; idx < value_segment.size(); ++idx) {
-
-  //}
-
-  for (int i = 0; i < _input_table_left()->chunk_count; i++) {
-    const auto& chunk = _input_table_left()->get_chunk(ChunkID(i));
+  for (ChunkID i = ChunkID(0); i < table->chunk_count(); i++) {
+    const auto& chunk = table->get_chunk(i);
     const auto& segment = chunk.get_segment(_column_id);
 
-    // Kann man den Typen direkt abfragen, dass man hier keinen vector
-    // durchgehen muss?
+    // The value vector of a ValueSegment is templated to match the
+    // data type stored in the column.
+    // That's why we use the resolve_data_type lambda expression
+    // to create a typed_segment that we can handle in the 
+    // not templated TableScan operator. (first approach in the pdf)
+    
+    // über ColumID bei segment nachgucken, welchen Typ
+    auto data_type = table->column_type(_column_id);
+    
+    //std::vector<std::string> possible_types{"int", "long", "float", "double", "string"};
+    //for(auto data_type : possible_types) {
+    resolve_data_type(data_type, [&] (auto type) {
+      using Type = typename decltype(type)::type;
 
-    std::vector<std::string> possible_types{"int", "long", "float", "double", "string"};
-    for(auto data_type : possible_types) {
-      resolve_data_type(data_type, [&] (auto type)) {
-        using Type = typename decltype(type)::type;
-        const auto value_segment = std::dynamic_pointer_cast<ValueSegment>(segment);
-        if(value_segment != nullptr) {
-          const auto& values = value_segment->values();
-          for (auto value : values) {
-            if ()
-          }
+      // is given segment a value segment?
+      const auto typed_segment = std::dynamic_pointer_cast<ValueSegment<Type>>(segment);
+      if(typed_segment != nullptr) {
+        PosList posList = PosList();
+
+        bool in_scope;
+        for (auto idx = size_t(0); idx < typed_segment->size(); idx++) {
+          auto values = typed_segment->values();
+          auto value = values.at(idx);
+          in_scope = comparator(_scan_type, value, type_cast<Type>search_value);
         }
       }
-    }
-    
+    });
+    //}
   }
 
   
